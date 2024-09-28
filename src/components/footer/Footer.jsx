d7241510fe7d746ad8fcc3fda0f78cbaaf223c4b
umedsingh98 <umedsingh0898@gmail.com>
2024-09-28 15:39:09 +0530

    Design Changes for Cart

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
       </div>
       <hr />
       <p className="footer-copyright">
-       Copyright 2024 HungerPoint. All rights reserved.
+       Copyright@2024 HungerPoint. All rights reserved.
       </p>
     </div>
   );
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -22,13 +22,49 @@ const Cart = () => {
 
           if(cartItems[foodItem._id] > 0){
             return(
-              <div className='cart-items-title cart-items-item'>
+              <>
+              <div className='cart-items-item'>
                 <img src={foodItem.image} alt="foodItemImage" />
                 <p>{foodItem.name}</p>
+                <p>Rs. {foodItem.price}</p>
+                <p>{cartItems[foodItem._id]}</p>
+                <p>Rs. {cartItems[foodItem._id] * foodItem.price}</p>
+                <p onClick={() => {removeFromCart(foodItem._id)}} className='cross'>X</p>
               </div>
+              <hr />
+              </>
             )
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-detail">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <p>Delivery</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+             <b>Total</b>
+             <b>{0}</b>
+            </div>
+          </div>
+          <button>Proceed To Checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <p>Have a Promocode?</p>
+          <div className="cart-promocode-input">
+            <input type="text" placeholder='Promo code'/>
+            <button>Submit</button>
+          </div>
+        </div>
       </div>
     </div>
   )
